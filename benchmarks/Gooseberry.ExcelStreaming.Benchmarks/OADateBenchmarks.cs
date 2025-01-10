15abf5f3cd135b7ddf7eda6600b6b49e747453b9
Aleс Anikin <alec.anikin@gmail.com>
2025-01-10 14:34:56 +0300

    AODate tests and benchmarks

---
+++ benchmarks/Gooseberry.ExcelStreaming.Benchmarks/OADateBenchmarks.cs
@@ -0,0 +1,51 @@
+﻿#if NET8_0_OR_GREATER
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using Gooseberry.ExcelStreaming.Writers;
+
+namespace Gooseberry.ExcelStreaming.Benchmarks;
+
+/*
+| Method                 | Mean      | Error    | StdDev   |
+|----------------------- |----------:|---------:|---------:|
+| CustomFormatterHandler |  13.48 ms | 0.113 ms | 0.101 ms |
+| DoubleFormatter        | 116.92 ms | 1.733 ms | 1.621 ms |
+*/
+
+[Orderer(SummaryOrderPolicy.FastestToSlowest)]
+public class OADateBenchmarks
+{
+    private static readonly DateTime Value = DateTime.Now;
+
+    [Benchmark]
+    public int CustomFormatterHandler()
+    {
+        var bytes = new byte[256];
+        int total = 0;
+
+        for (int i = 0; i < 1_000_000; ++i)
+        {
+            Utf8DateTimeCellWriter.FormatOADate(Value, bytes, out var written);
+            total += written;
+        }
+
+        return total;
+    }
+
+    [Benchmark]
+    public int DoubleFormatter()
+    {
+        var bytes = new byte[256];
+        int total = 0;
+
+        for (int i = 0; i < 1_000_000; ++i)
+        {
+            Value.ToOADate().TryFormat(bytes, out var written);
+
+            total += written;
+        }
+
+        return total;
+    }
+}
+#endif
--- tests/Gooseberry.ExcelStreaming.Tests/DateExtensionsTests.cs
+++ tests/Gooseberry.ExcelStreaming.Tests/DateExtensionsTests.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using System.Globalization;
+using FluentAssertions;
 using Gooseberry.ExcelStreaming.Extensions;
 using Gooseberry.ExcelStreaming.Writers;
 using Xunit;
@@ -23,4 +24,23 @@ public sealed class DateExtensionsTests
             date = date.AddDays(1);
         }
     }
+
+#if NET8_0_OR_GREATER
+    [Fact]
+    public void CheckCustomOADate()
+    {
+        var date = DateTime.Now.AddYears(-1500);
+
+        while (date.Year != 2025)
+        {
+            double internalOADate = double.Parse(date.ToInternalOADate(), NumberStyles.Any, CultureInfo.InvariantCulture);
+
+            double dateOADate = date.ToOADate();
+
+            DateTime.FromOADate(internalOADate).Should().BeCloseTo(DateTime.FromOADate(dateOADate), TimeSpan.FromMilliseconds(1));
+
+            date = date.AddDays(1).AddMicroseconds(Random.Shared.Next(1, 1_000));
+        }
+    }
+#endif
 }
